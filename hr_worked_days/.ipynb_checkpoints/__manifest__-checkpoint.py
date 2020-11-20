078e241659b5c6123e25e56ce75a7d9196d5816b
Nurlan Farajov <nurlan@erpgo.az>
2020-11-20 15:39:44 +0100

    add all to test

---
+++ hr_worked_days/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Dosttech Payslip Customizations",
+    'version': '0.1',
+    'depends': ['base','hr_holidays','hr_payroll_community'],
+    'author': "ERPGO",
+    'category': 'Uncategorized',
+    'summary': """Calculate working days excluding leaves in payslip""",
+    'description': """
+        Calculation of working days
+        Calculates the number of working days for a selected month in the payslip calculation.
+        Some use cases where this module can be useful:
+            *an employee hasn't worked the whole month and his basic pay needs to be prorated based on the actual worked days out of total worked days
+            
+            *use working days information for calculation in any other salary rule
+        
+        A float field has been added to the payslip form view which is calculating the working 
+        days in respective working schedule of an employee by talking out the respective global
+        leaves.""",
+    'website': "http://www.erpgo.az",
+    # always loaded
+    'data': [
+        'views/views.xml',
+    ],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    
+}
--- payroll_aze/__manifest__.py
+++ payroll_aze/__manifest__.py
@@ -20,12 +20,15 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','hr_payroll','worked_days'],
+    'depends': [
+                'base', 
+                'hr_worked_days',
+                'hr_payslip_monthly_report'
+                ],
 
     # always loaded
     'data': [
 #         'security/ir.model.access.csv',
-        'views/hr_contract.xml',
         'views/hr_contract.xml',
         'views/hr_employee.xml'
     ],
